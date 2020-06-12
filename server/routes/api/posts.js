216a189036405682d6fd7fc1dc9d9a19594013ff
kepha <kephamotari1@gmail.com>
2020-06-12 11:42:37 +0300

    Initial backend created

---
+++ server/routes/api/posts.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const mongodb = require("mongodb");
+
+const router = express.Router();
+
+//Get Posts
+router.get("/", async (req, res) => {
+  const posts = await loadPostsCollection();
+  res.send(await posts.find({}).toArray());
+});
+
+//Add Post
+router.post("/", async (req, res) => {
+  const posts = await loadPostsCollection();
+  await posts.insertOne({
+    text: req.body.text,
+    createAt: new Date(),
+  });
+  res.status(201).send();
+});
+
+//Delete Post
+router.delete("/:id", async (req, res) => {
+  const posts = await loadPostsCollection();
+  await posts.deleteOne({ _id: new mongodb.ObjectID(req.params.id) });
+  res.status(200).send();
+});
+
+async function loadPostsCollection() {
+  const client = await mongodb.MongoClient.connect(
+    "mongodb+srv://mean123:mean123@cluster0-qqyjh.mongodb.net/<dbname>?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+    }
+  );
+
+  return client.db("vue_express").collection("posts");
+}
+
+module.exports = router;
